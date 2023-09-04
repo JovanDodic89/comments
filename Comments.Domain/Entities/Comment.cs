a7b518e1ecefaa96005eb5ae5161094290ee314d
tuxgg <tuxgg@DESKTOP-BC2ER32>
2023-09-04 22:52:14 +0200

    added domain layer added interfaces folder added entities foder

---
+++ Comments.Domain/Entities/Comment.cs
@@ -0,0 +1,15 @@
+﻿namespace Comments.Domain.Entities
+{
+    public class Comment
+    {
+        public string Id { get; set; }
+        public string ParentId { get; set; }
+        public string Context { get; set; }
+        public string UserId { get; set; }
+        public string UserDisplayName { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string Text { get; set; }
+        public DateTime? LastModified { get; set; }
+        public int ReplyCount { get; set; }
+    }
+}
---
+++ Comments.Domain/Interfaces/ICommentsRepository.cs
@@ -0,0 +1,14 @@
+﻿using Comments.Domain.Entities;
+using System.Threading;
+
+namespace Comments.Domain.Interfaces
+{
+    public interface ICommentsRepository
+    {
+        public Task<Comment> AddComment(Comment comment);
+        public Task<Comment> UpdateComment(string id, string text);
+        public Task<Comment> GetComment(string context);
+        public Task<int> GetCommentDepth(Comment comment);
+        public Task<bool> DeleteComment(string id);
+    }
+}
